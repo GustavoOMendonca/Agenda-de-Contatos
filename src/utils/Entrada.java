9072f6db4b4c7d3ff42670704ac303db17d44b86
Oliver <gustavodeoliveira3443@gmail.com>
2026-05-17 14:01:47 -0300

    feat: cria classe entrada

--- src/utils/Entrada.java
+++ src/utils/Entrada.java
@@ -1,4 +1,22 @@
 package utils;
 
+import java.util.Scanner;
+
 public class Entrada {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String lerTexto(String mensagem) {
+
+        System.out.print(mensagem);
+
+        return scanner.nextLine();
+    }
+
+    public static int lerInt(String mensagem) {
+
+        System.out.print(mensagem);
+
+        return Integer.parseInt(scanner.nextLine());
+    }
 }
